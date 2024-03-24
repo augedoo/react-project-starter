851d805cffa5ed22aacbff6ea9373f58a978c7ed
Augustine Amoh Nkansah <>
2024-03-24 12:34:29 +0000

    Initial project starter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {},
+				secondary: {},
+			},
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/forms')({
+			strategy: 'class',
+		}),
+	],
 };
